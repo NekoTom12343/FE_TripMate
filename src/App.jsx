60c383f38ee4cff2ec7bf3abb29da36febe6689b
NekoTom12343 <tommeo2014@gmail.com>
2024-10-05 23:27:01 +0700

    add: route for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
 import "./App.css";
 import TravelLogin from "./components/TravelLogin";
+import TravelRegister from "./components/TravelRegister";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <TravelLogin />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<TravelLogin />} />
+          <Route path="login" element={<TravelLogin />} />
+          <Route path="register" element={<TravelRegister />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
