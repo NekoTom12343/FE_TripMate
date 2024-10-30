e690656d940766117f0a05f927b84f9c46d155ca
NekoTom12343 <tommeo2014@gmail.com>
2024-10-30 07:51:21 +0700

    update: getUserMeta api

--- src/apis/user.js
+++ src/apis/user.js
@@ -2,12 +2,12 @@ import apis from ".";
 
 export const getUserMeta = async (token) => {
   try {
-    const response = await apis.get("/user/info", {
+    const response = await apis.get("/users/info", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return response.result;
+    return response.data;
   } catch (error) {
     throw error.response ? error.response.result : error;
   }
