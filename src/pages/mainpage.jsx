c0da6ebc829ee4763e7a44015200cc662397430f
NekoTom12343 <tommeo2014@gmail.com>
2024-10-31 19:41:50 +0700

    add: banner and filterbar

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -1,9 +1,13 @@
+import FilterBar from "../components/FilterBar";
 import HeaderBar from "../components/HeaderBar";
+import HomePageBanner from "../components/HomePageBanner";
 
 export default function Mainpage() {
   return (
     <div>
       <HeaderBar />
+      <HomePageBanner />
+      <FilterBar />
     </div>
   );
 }
