8feb540e46c2b3ab5900d03f4fa83d117511e613
NekoTom12343 <tommeo2014@gmail.com>
2025-01-19 23:24:54 +0700

    create: TopDestination component

---
+++ src/components/TopDestinations.jsx
@@ -0,0 +1,14 @@
+export default function TopDestinations() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        backgroundColor: "#F5F9FF",
+        textAlign: "left",
+        fontFamily: "Urbanist",
+      }}
+    >
+      <div className="mt-10 mb-10 mx-10 space-y-16"></div>
+    </div>
+  );
+}
