1b4e0ac013d33eed11ef91f73a47a2c9d2b0db20
NekoTom12343 <tommeo2014@gmail.com>
2025-02-14 00:10:50 +0700

    update: change route TravelGuide

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AuthGuard from "./middleware/AuthGuard";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import Mainpage from "./pages/mainpage";
 import LocationPage from "./pages/LocationPage";
-import TravelGuideBlog from "./components/TravelGuideBlog";
+import TravelGuidePage from "./pages/TravelGuidePage";
 function App() {
   return (
     <BrowserRouter>
@@ -22,7 +22,7 @@ function App() {
           /> */}
           <Route path="home" element={<Mainpage />} />
           <Route path="location/:locationId" element={<LocationPage />} />
-          <Route path="test" element={<TravelGuideBlog />} />
+          <Route path="travelguide/:guideId" element={<TravelGuidePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
